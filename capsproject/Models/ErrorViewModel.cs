e464721184eb5edfed7396fa32e881d32f93dfa6
Yoojavaa <kenlesterbernaldez24@gmail.com>
2026-06-27 17:44:17 +0800

    initial commit

---
+++ capsproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace capsproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
